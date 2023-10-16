8408ee1405f263201598067804dd4eb880160100
AmirInt <amir.intelli@gmail.com>
2023-10-16 21:11:42 +0100

    Develop module image_retriever using OpenCV.

--- traffix/vision/image_retriever.py
+++ traffix/vision/image_retriever.py
@@ -1,12 +1,66 @@
-import cv2
+import sys
+import numpy as np
+import cv2 as cv
 
 
 class ImageRetriever:
-    def __init__(self, source=""):
-
+    def __init__(self, source="", history_size=2e+8):
         if source == "":
-            pass
+            self.__source = cv.VideoCapture(0)
+        else:
+            self.__source = cv.VideoCapture(source)
+
+        if not self.__source.isOpened():
+            print("Could not access video source")
+            exit()
+
+        cv.namedWindow("source", cv.WINDOW_AUTOSIZE)
+
+        self.__frames = []
+
+        self.__history_size = history_size
+
+    
+    def play_source(self, display=False):
+
+        try:
+            while True:
+                ret, frame = self.__source.read()
+
+                if not ret:
+                    print("Could not receive stream frame. Exiting...")
+                    break
+                
+                rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+                if display:
+                    cv.imshow("source", rgb_frame)
+
+                if cv.waitKey(1) == ord('q'):
+                    break
+
+        except KeyboardInterrupt:
+            self.stop()
 
 
+    def append_to_history(self, frame: np.ndarray):
+        self.__frames.append(frame)
+        while sys.getsizeof(self.__frames) > self.__history_size:
+            self.__frames.pop(0)
 
 
+    def set_history_size(self, history_size):
+        self.__history_size = history_size
+
+
+    def get_last_frame(self):
+        return self.__frames[-1]
+
+
+    def get_all_frames(self):
+        return self.__frames
+
+
+    def stop(self):
+        self.__source.release()
+        cv.destroyAllWindows()
