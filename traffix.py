9446d24f5427725679131d5cc4df53a6bbc7d204
AmirInt <amir.intelli@gmail.com>
2023-10-18 21:39:59 +0100

    Get video images. && Fix video FPS. && Fix memory clean-up management.

--- traffix.py
+++ traffix.py
@@ -1,1 +1,12 @@
+import sys
+import threading
 
+from traffix.vision.image_retriever import ImageRetriever
+
+
+if __name__ == "__main__":
+    video = ImageRetriever(source=sys.argv[1], fps=float(sys.argv[2]))
+
+    video.play_source(True)
+
+    video.stop()
--- traffix/vision/image_retriever.py
+++ traffix/vision/image_retriever.py
@@ -1,10 +1,14 @@
+import os
 import sys
 import numpy as np
 import cv2 as cv
 
+from datetime import datetime
+from pympler.asizeof import asizeof
+
 
 class ImageRetriever:
-    def __init__(self, source="", history_size=2e+8):
+    def __init__(self, source="", history_size=2e+8, fps=30.0):
         if source == "":
             self.__source = cv.VideoCapture(0)
         else:
@@ -18,12 +22,18 @@ class ImageRetriever:
 
         self.__frames = []
 
-        self.__history_size = history_size
+        self.__history_size = int(history_size)
+
+        self.__fps = fps
 
     
     def play_source(self, display=False):
 
         try:
+            legit_wait_time = int(1.0 / self.__fps * 1000)
+            wait_time = legit_wait_time
+            frames_read = 0
+            start_time = datetime.now()
             while True:
                 ret, frame = self.__source.read()
 
@@ -36,16 +46,27 @@ class ImageRetriever:
                 if display:
                     cv.imshow("source", rgb_frame)
 
-                if cv.waitKey(1) == ord('q'):
+                self.append_to_history(frame)
+
+                if cv.waitKey(wait_time) == ord('q'):
                     break
+                
+                frames_read += 1
+                time_delta = (datetime.now() - start_time).total_seconds()
+                current_fps = float(frames_read) / time_delta
+
+                wait_time = legit_wait_time + 10 * int(current_fps - self.__fps)
+                if wait_time <= 0:
+                    wait_time = 3
 
         except KeyboardInterrupt:
+            print("Exiting...")
             self.stop()
 
 
     def append_to_history(self, frame: np.ndarray):
         self.__frames.append(frame)
-        while sys.getsizeof(self.__frames) > self.__history_size:
+        while asizeof(self.__frames) > self.__history_size:
             self.__frames.pop(0)
 
 
