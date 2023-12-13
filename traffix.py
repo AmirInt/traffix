bb4e0c5f4f55489362a90fa984077aa23e5b83d6
AmirInt <amir.intelli@gmail.com>
2023-12-13 09:41:30 +0000

    Modify ROI for video sources. && Assemble modules.

--- traffix.py
+++ traffix.py
@@ -51,59 +51,57 @@ if __name__ == "__main__":
                 Thread(target=vehicle_counters[-1].process_source,
                        args=[videos[-1]]))
         
-            vehicle_counter_threads[-1].daemon = True
-            vehicle_counter_threads[-1].start()
-        
-        ## Initialise traffic count data and predictors
-        #history_depth = config["traffic_predictor"]["history_depth"]
-        #prediction_depth = config["traffic_predictor"]["prediction_depth"]
-        #hidden_size= config["traffic_predictor"]["hidden_size"]
-        #num_stacked_layers = config["traffic_predictor"]["num_stacked_layers"]
+        # Initialise traffic count data and predictors
+        history_depth = config["traffic_predictor"]["history_depth"]
+        prediction_depth = config["traffic_predictor"]["prediction_depth"]
+        hidden_size= config["traffic_predictor"]["hidden_size"]
+        num_stacked_layers = config["traffic_predictor"]["num_stacked_layers"]
 
-        #for predictor in config["traffic_predictors"]:
-        #    traffic_count_recorders.append(DataProcessor(
-        #        data_csv=predictor["initial_data"],
-        #        n_lookback=history_depth,
-        #        n_predict=prediction_depth,
-        #        index_title="index",
-        #        vehicle_count_title="vehicle_count"))
+        for predictor in config["traffic_predictors"]:
+            traffic_count_recorders.append(DataProcessor(
+                data_csv=predictor["initial_data"],
+                n_lookback=history_depth,
+                n_predict=prediction_depth,
+                index_title="index",
+                vehicle_count_title="vehicle_count"))
             
-        #    traffic_count_recorders[-1].fit_self()
+            traffic_count_recorders[-1].fit_self()
 
-        #    traffic_predictors.append(Predictor(
-        #        1,
-        #        output_size=prediction_depth,
-        #        hidden_size=hidden_size,
-        #        num_stacked_layers=num_stacked_layers,
-        #        n_lookback=history_depth,
-        #        n_predict=prediction_depth))
+            traffic_predictors.append(Predictor(
+                1,
+                output_size=prediction_depth,
+                hidden_size=hidden_size,
+                num_stacked_layers=num_stacked_layers,
+                n_lookback=history_depth,
+                n_predict=prediction_depth))
 
-        #    traffic_predictors[-1].load_model(predictor["model_weights"])
+            traffic_predictors[-1].load_model(predictor["model_weights"])
 
-        ## Initialise the scheduler module
-        #paths = []
-        #for i in range(config["scheduler"]["paths"]):
-        #    path = []
-        #    for line_idx in config["scheduler"][f"path_{i}"]:
-        #        line = (vehicle_counters[line_idx],
-        #                traffic_count_recorders[line_idx],
-        #                traffic_predictors[line_idx])
-        #        path.append(line)
-        #    paths.append(path)
+        # Initialise the scheduler module
+        paths = []
+        for i in range(config["scheduler"]["paths"]):
+            path = []
+            for line_idx in config["scheduler"][f"path_{i}"]:
+                line = (vehicle_counters[line_idx],
+                        traffic_count_recorders[line_idx],
+                        traffic_predictors[line_idx])
+                path.append(line)
+            paths.append(path)
 
-        #scheduler = Scheduler(paths,
-        #                      config["scheduler"]["schedule_period"],
-        #                      history_depth,
-        #                      prediction_depth)
+        scheduler = Scheduler(paths,
+                              config["scheduler"]["schedule_period"],
+                              history_depth,
+                              prediction_depth)
 
         for video_thread in video_threads:
             video_thread.daemon = True
             video_thread.start()
 
-        #scheduler.run_schedule()
+        for vehicle_counter_thread in vehicle_counter_threads:
+            vehicle_counter_thread.daemon = True
+            vehicle_counter_thread.start()
 
-        while True:
-            sleep(10.0)
+        scheduler.run_schedule()
 
     except KeyboardInterrupt:
         print("Stopping vehicle counters...")
